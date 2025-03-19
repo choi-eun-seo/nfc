b12f5f18723824877fd5614d33da1ccc9f93b316
최은서 <choieunseo@choeeunseoui-MacBookPro.local>
2025-03-20 01:20:06 +0900

    Feat: asdf

--- next.config.ts
+++ next.config.ts
@@ -1,56 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  /* config options here */
-
-  webpack(config) {
-    // Grab the existing rule that handles SVG imports
-
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg"),
-    );
-
-    const svgrQuery = /comp/;
-
-    config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: {
-          not: [...fileLoaderRule.resourceQuery.not, svgrQuery],
-        }, // exclude if *.svg?url
-      },
-      // Convert all other *.svg imports to React components
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: svgrQuery, // *.svg?url
-        use: [
-          {
-            loader: "@svgr/webpack",
-            options: {
-              icon: true,
-              replaceAttrValues: {
-                "#000": "{props.color ?? 'currentColor'}",
-              },
-            },
-          },
-        ],
-      },
-    );
-
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    return config;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  experimental: {
+    appDir: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
