3afa21bcd7f2929d49bcfb2e810b8d7d343236c3
choi-eun-seo <sshses113@gmail.com>
2025-03-20 11:33:05 +0900

    Feat: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <h1>🏠 홈 페이지</h1>
       <p>이곳은 Next.js 애플리케이션의 메인 페이지입니다.</p>
       <Link href="/nfc">📡 NFC 페이지로 이동</Link>
+      <Link href="/qr">QR 페이지로 이동</Link>
     </div>
   );
 }
